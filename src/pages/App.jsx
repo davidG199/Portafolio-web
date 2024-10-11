c2afab42086027fe60c9e21fa291596cbea955d2
David gomez <david.gomez04@hotmail.com>
2024-10-11 14:27:37 -0500

    scroll to top

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,18 +1,28 @@
-import '../assets/App.css'
-import { Route, Routes } from "react-router-dom";
-import Home from '../pages/home'
-import PageProject from './pageProjects';
+import "../assets/App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "../pages/home";
+import PageProject from "./pageProjects";
+import { useEffect } from "react";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 function App() {
-
   return (
     <>
+      <ScrollToTop />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/ProjectsPage' element={<PageProject/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/ProjectsPage" element={<PageProject />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
